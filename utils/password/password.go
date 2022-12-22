0dde3aeb79b23d5a712c3f21d9d687a468d0b943
Vindy Friendly Valentine <vindyfriendly@gmail.com>
2022-12-22 19:41:05 +0800

    Refactoring

--- utils/password/password.go
+++ utils/password/password.go
@@ -4,11 +4,6 @@ import (
 	"github.com/alexedwards/argon2id"
 )
 
-type Hasher interface {
-	GeneratePassword(password string) (hashedPassword string, err error)
-	VerifyPassword(password string, hashedPassword string) (match bool, err error)
-}
-
 func GeneratePassword(password string) (hashedPassword string, err error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
 }
